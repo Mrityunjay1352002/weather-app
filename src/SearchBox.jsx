bf5b9dfb6570660bce49c1867edfa5194f214642
Mrityunjay <mrityunjaykumar08805@gmail.com>
2025-10-30 00:50:40 +0530

    Fix: use HTTPS and ensure VITE_API_KEY in .env

--- src/SearchBox.jsx
+++ src/SearchBox.jsx
@@ -6,7 +6,7 @@ import { useState } from 'react';
 export default function SearchBox ({ updateInfo }) {
     let [city, setCity] = useState("");
     let [error, setError] = useState(false);
-    const API_URL = "http://api.openweathermap.org/geo/1.0/direct";
+    const API_URL = "https://api.openweathermap.org/geo/1.0/direct";
     const API_KEY = import.meta.env.VITE_API_KEY;
     // const API_KEY = "de0cc7fd25c0c850f39e1e56060ad7e0";
 
